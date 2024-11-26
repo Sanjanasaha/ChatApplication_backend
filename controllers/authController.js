911502deed51bc14307dd90d6c206dfa25cacc88
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-11-26 18:20:33 +0530

    riju

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,83 +1,99 @@
 import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
 import generateTokenAndSetCookie from "../utils/generateTokens.js";
+import jwt from "jsonwebtoken";
 
-export const signup=async(req,res)=>{
-    try {
-        const {fullName,userName,password,confirmPassword,gender}=req.body;
-        if(password!==confirmPassword){
-            return res.status(400).json({error:"Passwords don't match"});
-        }
-        const user= await User.findOne({userName});
-        if(user){
-            return res.status(400).json({error:"Username already exists"});
-        }
-        //hash password
-        const salt=await bcrypt.genSalt(10);
-        const hashedPassword=await bcrypt.hash(password,salt);
-
-        
-        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
-        const girlProfilePic = `http://avatar.iran.liara.run/public/girl?username=${userName}`;
-        // console.log('Generated Profile Pic URL:', gender === "male" ? boyProfilePic : girlProfilePic);
-        const newUser=new User({
-            fullName,
-            userName,
-            password:hashedPassword,
-            gender,
-            profilePic:gender==="male"?boyProfilePic:girlProfilePic,
-        });
-        
-        if(newUser){
-            //generate jwt token
-            generateTokenAndSetCookie(newUser._id,res);
-            
-            await newUser.save();
-            
-            res.status(201).json({
-                _id:newUser._id,
-                fullName:newUser.fullName,
-                userName:newUser.userName,
-                profilePic:newUser.profilePic
-                
-            });
-        }else{
-            res.status(400).json({error:"Invalid user data"});
-        }
-        
-    } catch (error) {
-        console.log("Error in signUp controller",error.message);
-        res.status(500).json({error:"Internal Server Error"});
+export const signup = async (req, res) => {
+  try {
+    const { fullName, userName, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
     }
+    const user = await User.findOne({ userName });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlProfilePic = `http://avatar.iran.liara.run/public/girl?username=${userName}`;
+    // console.log('Generated Profile Pic URL:', gender === "male" ? boyProfilePic : girlProfilePic);
+    const newUser = new User({
+      fullName,
+      userName,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      //generate jwt token
+      generateTokenAndSetCookie(newUser._id, res);
+
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName: newUser.userName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signUp controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 ////oiL1YtUUlBuza5YB
 //sanjanasaha26
-export const login=async(req,res)=>{
-    try {
-		const { userName, password } = req.body;
-		const user = await User.findOne({ userName });
-		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
-		if (!user || !isPasswordCorrect) {
-			return res.status(400).json({ error: "Invalid username or password" });
-		}
-		generateTokenAndSetCookie(user._id, res);
-		res.status(200).json({
-			_id: user._id,
-			fullName: user.fullName,
-			userName: user.userName,
-			profilePic: user.profilePic,
-		});
-	} catch (error) {
-		console.log("Error in login controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
-}
-export const logout=(req,res)=>{
-    try {
-        res.cookie("jwt", "", { maxAge: 0 });
-		res.status(200).json({ message: "Logged out successfully" });
-    } catch (error) {
-        console.log("Error in logout controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
+export const login = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
     }
-}
+
+    const expiresIn = 60 * 60 * 24 * 30;
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: expiresIn,
+    });
+
+    const exp = Date.now() + expiresIn * 1000;
+    res.cookie("jwt", token, {
+      expires: new Date(exp),
+      httpOnly: true,
+      sameSite: "lax",
+      secure: process.env.NODE_ENV !== "development",
+      path: "/",
+    });
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      userName: user.userName,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/authRoutes.js";
 import msgRoutes from "./routes/msgRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import {app,server} from "./socket/socket.js"
+import { app, server } from "./socket/socket.js";
 
 import cors from "cors";
 
@@ -16,17 +16,16 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-      origin: "*", // Allow all origins (change this in production)
-      optionsSuccessStatus: 200,
-    })
-  );
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",msgRoutes);
-app.use("/api/users",userRoutes);
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", msgRoutes);
+app.use("/api/users", userRoutes);
 
-
-server.listen(PORT,()=>{
-    connectToMongoDB();
-    console.log(`Server is running on port ${PORT}`
-)});
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
